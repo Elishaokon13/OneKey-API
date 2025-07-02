c8689d42187bef96bf1bef8234d937b2078ae171
Elishaokon13 <eliscodavid@gmail.com>
2025-07-02 22:16:56 +0100

    Modified dist/config/environment.js

--- dist/config/environment.js
+++ dist/config/environment.js
@@ -3,10 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 // Load environment variables
 dotenv_1.default.config();
 const config = {
+    api: {
+        version: process.env.API_VERSION || '1.0.0',
+    },
     server: {
         port: parseInt(process.env.PORT || '3000', 10),
         nodeEnv: process.env.NODE_ENV || 'development',
@@ -45,6 +49,12 @@ const config = {
     blockchain: {
         privyAppId: process.env.PRIVY_APP_ID || '',
         privyAppSecret: process.env.PRIVY_APP_SECRET || '',
+        chainId: parseInt(process.env.BLOCKCHAIN_CHAIN_ID || '84532', 10), // Base Sepolia
+        rpcUrl: process.env.BLOCKCHAIN_RPC_URL || 'https://sepolia.base.org',
+        easContractAddress: process.env.EAS_CONTRACT_ADDRESS || '0x4200000000000000000000000000000000000021',
+        easSchemaRegistryAddress: process.env.EAS_SCHEMA_REGISTRY_ADDRESS || '0x4200000000000000000000000000000000000020',
+        attesterPrivateKey: process.env.ATTESTER_PRIVATE_KEY || '',
+        attesterAddress: process.env.ATTESTER_ADDRESS || '',
         easSchemaId: process.env.EAS_SCHEMA_ID || '',
         easAttestationUrl: process.env.EAS_ATTESTATION_URL || 'https://base-sepolia.easscan.org',
     },
@@ -59,7 +69,8 @@ const config = {
         rateLimitMaxRequests: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS || '100', 10),
         encryptionKey: process.env.ENCRYPTION_KEY || 'dev-encryption-key-change-in-production',
         encryptionIv: process.env.ENCRYPTION_IV || 'dev-encryption-iv-change-in-production',
+        hashSalt: process.env.HASH_SALT || 'dev-hash-salt-change-in-production',
     },
 };
-exports.default = config;
+exports.config = config;
 //# sourceMappingURL=environment.js.map
