fcf5786e80210a95575602424a114716dabfd8fe
Elishaokon13 <eliscodavid@gmail.com>
2025-07-02 22:16:57 +0100

    Modified dist/services/kyc/onfidoService.d.ts

--- dist/services/kyc/onfidoService.d.ts
+++ dist/services/kyc/onfidoService.d.ts
@@ -1,5 +1,5 @@
 import { BaseKycService } from './baseKycService';
-import { KycSession, KycVerificationResult, CreateKycSessionRequest, KycProvider } from '../../types/kyc';
+import { KycSession, KycVerificationResult, CreateKycSessionRequest, KycProvider } from '@/types/kyc';
 export declare class OnfidoService extends BaseKycService {
     provider: KycProvider;
     constructor();
