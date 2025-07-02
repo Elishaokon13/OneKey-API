80021523770ba44b82e50f8651cbaab2db5a7c79
Elishaokon13 <eliscodavid@gmail.com>
2025-07-02 22:16:57 +0100

    Modified dist/services/kyc/smileIdentityService.d.ts

--- dist/services/kyc/smileIdentityService.d.ts
+++ dist/services/kyc/smileIdentityService.d.ts
@@ -1,5 +1,5 @@
 import { BaseKycService } from './baseKycService';
-import { KycSession, KycVerificationResult, CreateKycSessionRequest, KycProvider } from '../../types/kyc';
+import { KycSession, KycVerificationResult, CreateKycSessionRequest, KycProvider } from '@/types/kyc';
 export declare class SmileIdentityService extends BaseKycService {
     provider: KycProvider;
     constructor();
