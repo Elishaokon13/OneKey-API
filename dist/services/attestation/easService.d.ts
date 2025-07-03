5d6a9c0c8c4a97f3a3286e16de4f7b6e6b6db68f
Elishaokon13 <eliscodavid@gmail.com>
2025-07-03 23:59:30 +0100

    Modified dist/services/attestation/easService.d.ts

--- dist/services/attestation/easService.d.ts
+++ dist/services/attestation/easService.d.ts
@@ -1,12 +1,21 @@
 import { BaseAttestationService } from './baseAttestationService';
 import { EasAttestation, CreateAttestationRequest, AttestationVerificationResult, GasEstimate, EasConfig } from '../../types/attestation';
 export declare class EasService extends BaseAttestationService {
+    private readonly arweaveConfig?;
     private eas;
     private schemaEncoder;
+    private schemaManager;
+    private arweaveService?;
     private readonly SCHEMA_DEFINITION;
-    constructor(easConfig: EasConfig);
+    constructor(easConfig: EasConfig, arweaveConfig?: any | undefined);
     protected initializeProvider(): Promise<void>;
+    private validateDefaultSchema;
     protected createAttestation(request: CreateAttestationRequest): Promise<EasAttestation>;
+    createBatchAttestations(requests: CreateAttestationRequest[]): Promise<EasAttestation[]>;
+    private processBatchReceipt;
+    private extractBatchAttestationUids;
+    private executeWithRetry;
+    private shouldRetry;
     protected verifyOnChain(uid: string): Promise<AttestationVerificationResult>;
     protected estimateGas(request: CreateAttestationRequest): Promise<GasEstimate>;
     revokeAttestation(uid: string, reason?: string): Promise<boolean>;
@@ -16,6 +25,8 @@ export declare class EasService extends BaseAttestationService {
     private getVerificationErrors;
     private estimateConfirmationTime;
     private storeAttestation;
-    private updateAttestationStatus;
+    private storeAttestationInDb;
+    private updateAttestationInDb;
+    private validateRequest;
 }
 //# sourceMappingURL=easService.d.ts.map
