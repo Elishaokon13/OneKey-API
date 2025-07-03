d7d4d2d3fe117b34b1f2e95f4b202c8705c9872d
Elishaokon13 <eliscodavid@gmail.com>
2025-07-03 23:59:54 +0100

    Modified dist/routes/attestation.d.ts

--- dist/routes/attestation.d.ts
+++
@@ -1,6 +0,0 @@
-import { AttestationService } from '../services/attestation/attestationService';
-declare const router: import("express-serve-static-core").Router;
-declare const attestationService: AttestationService;
-export { router as attestationRoutes };
-export { attestationService };
-//# sourceMappingURL=attestation.d.ts.map
