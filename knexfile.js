89fee2d6fd534efff7644d156b955b2f1ad6eb92
Elishaokon13 <eliscodavid@gmail.com>
2025-07-06 23:36:19 +0100

    Modified knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,1 +1,36 @@
- 
+// Import environment configuration
+require('ts-node/register');
+const { config } = require('./src/config/environment');
+
+const baseConfig = {
+  client: 'postgresql',
+  migrations: {
+    directory: './src/migrations',
+    extension: 'ts'
+  }
+};
+
+module.exports = {
+  development: {
+    ...baseConfig,
+    connection: {
+      host: config.database.host,
+      port: config.database.port,
+      database: config.database.name,
+      user: config.database.user,
+      password: config.database.password,
+      ssl: false
+    }
+  },
+  production: {
+    ...baseConfig,
+    connection: {
+      host: config.database.host,
+      port: config.database.port,
+      database: config.database.name,
+      user: config.database.user,
+      password: config.database.password,
+      ssl: { rejectUnauthorized: false }
+    }
+  }
+}; 
