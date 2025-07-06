3ab67c364d4ce0ecf41b2b5c508ab2f3cdbc01df
Elishaokon13 <eliscodavid@gmail.com>
2025-07-06 02:17:07 +0100

    Modified src/utils/errors.ts

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,1 +1,49 @@
- 
+export class BaseError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends BaseError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class ValidationError extends BaseError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class DatabaseError extends BaseError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class AuthenticationError extends BaseError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class AuthorizationError extends BaseError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class RateLimitError extends BaseError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class ConfigurationError extends BaseError {
+  constructor(message: string) {
+    super(message);
+  }
+} 
