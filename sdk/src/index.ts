892c330df3a2d2258f8d6d50e86be79abafa7097
Elishaokon13 <eliscodavid@gmail.com>
2025-07-13 14:41:01 +0100

    Modified sdk/src/index.ts

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -2,6 +2,10 @@
 export { OneKeySDK, createOneKeySDK } from './core/onekey-sdk';
 export { HttpClient } from './core/http-client';
 
+// Module exports
+export { KycClient, createKycClient } from './kyc/kyc-client';
+export * from './crypto';
+
 // Type exports
 export * from './types';
 
