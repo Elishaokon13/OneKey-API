ca77599bd9303cb311a789851ffe49d33d4568f3
Elishaokon13 <eliscodavid@gmail.com>
2025-07-13 14:35:38 +0100

    Modified sdk/src/index.ts

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,1 +1,15 @@
- 
+// Main SDK exports
+export { OneKeySDK, createOneKeySDK } from './core/onekey-sdk';
+export { HttpClient } from './core/http-client';
+
+// Type exports
+export * from './types';
+
+// Utility exports
+export { default as OneKeyError } from './utils/errors';
+
+// Version info
+export const VERSION = '1.0.0';
+
+// Default export - the main SDK class
+export { OneKeySDK as default } from './core/onekey-sdk'; 
